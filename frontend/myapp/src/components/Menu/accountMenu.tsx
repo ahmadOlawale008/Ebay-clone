34de916c8e416421948b39c0909b0bf3621c989e
ahmadOlawale008 <kassimolwale15@gmail.com>
2024-05-17 23:07:04 +0100

    Navbar extended

--- frontend/myapp/src/components/Menu/accountMenu.tsx
+++ frontend/myapp/src/components/Menu/accountMenu.tsx
@@ -1,13 +1,20 @@
+import React, { ReactNode, useRef } from "react"
 import SettingsIcon from "../../assets/icons/settingsIcon"
 import UserIcon from "../../assets/icons/userIcon"
-import {motion} from "framer-motion"
-const AccountMenu = () => {
+import { motion } from "framer-motion"
+import "./account-menu.css"
+interface AccountMenuProps{
+    anchorOrigin?: HTMLDivElement | null,
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+const AccountMenu: React.FC<AccountMenuProps> = () => {
+    const popMenuRef = useRef(null)
     return (
-        <div className="absolute top-0 translate-x-[-13px] shadow-xl translate-y-[30px] rounded-lg z-20 right-0 bg-slate-200 min-w-[350px]  py-5">
+        <div ref={popMenuRef} className="absolute top-0 overflow-hidden translate-x-[-13px] shadow-xl  translate-y-[30px] rounded-2xl z-20 right-0 bg-slate-200 min-w-[350px]  pb-8">
             <div className="w-full mx-auto  items-center">
-                <div className="w-full flex flex-col gap-y-4 justify-center mx-auto">
-                    <span className='border ring-2 ring-slate-300 inline-flex items-center justify-center m-auto size-16 rounded-full bg-slate-500 p-1 '>
-                        <UserIcon className='size-16 content-[1] relative cursor-pointer fill-white' />
+                <div className="w-full flex flex-col ashau8 gap-y-4 justify-center mx-auto">
+                    <span className='border ring-2 ring-slate-300 inline-flex items-center justify-center m-auto size-20 rounded-full bg-slate-500 p-1 '>
+                        <UserIcon className=' w-full content-[1] relative cursor-pointer fill-white' />
                     </span>
                     <div className="text-center tracking-wide block">
                         <p className="text-center text-lg ">
@@ -19,37 +26,52 @@ const AccountMenu = () => {
                     </div>
                 </div>
                 <div className="my-3">
-                    <div className="px-2 items-center gap-x-3 flex bg-slate-300 py-3">
+                    <div className="px-2 items-center gap-x-3 flex cursor-pointer hover:bg-slate-300 py-3">
+                        <UserIcon className="size-8 fill-slate-400" />
+                        <div className="">
+                            View your profile
+                        </div>
+                    </div>
+                    <div className="px-2 items-center gap-x-3 flex cursor-pointer hover:bg-slate-300 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8 stroke-slate-400">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                        </svg>
+                        <div className="">
+                            Favorites
+                        </div>
+                    </div>
+                    <hr className="bg-slate-700/20 h-[2px]" />
+
+                    <div className="px-2 items-center gap-x-3 flex cursor-pointer hover:bg-slate-300 py-3">
                         <SettingsIcon className="size-8 fill-slate-400" />
                         <div className="">
                             Settings
                         </div>
                     </div>
-                    <hr />
-                    <div className="px-2 items-center gap-x-3 flex bg-slate-300 py-3">
-                        <SettingsIcon className="size-8 fill-slate-400" />
+                    <div className="px-2 items-center gap-x-3 flex cursor-pointer hover:bg-slate-300 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 stroke-slate-400">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                        </svg>
                         <div className="">
-                            Settings
+                            Logout
                         </div>
                     </div>
-                    <div className="px-2 items-center gap-x-3 flex bg-slate-300 py-3">
-                        <motion.div className="capsule-login size-16 rounded-full flex items-center justify-center bg-slate-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
-                            </svg>
-                        </motion.div>
-                    </div>
+                    <hr className="bg-slate-700/20 h-[2px]" />
                 </div>
-                <div className="capsule w-full cursor-pointer  rounded-full mt-6 bg-slate-300">
-                    <motion.div className="capsule-login size-16 rounded-full flex items-center justify-center bg-slate-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <div className="px-2 gap-x-3 flex  items-baseline justify-between ">
+                    <div className="flex items-center gap-x-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 fill-slate-400">
                             <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
                         </svg>
-                    </motion.div>
+                        <small className="text-base basis-1/4 text-nowrap">Dark Mode</small>
+                    </div>
+                    <div className="capsule shadow-md basis-1/5 w-full cursor-pointer  rounded-full mt-6 bg-slate-300">
+                        <motion.div className="capsule-login size-10 rounded-full flex items-center justify-center bg-gray-400">
+                        </motion.div>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
-
 export default AccountMenu
--- frontend/myapp/src/components/Navbar/NavbarInto.tsx
+++ frontend/myapp/src/components/Navbar/NavbarInto.tsx
@@ -1,8 +1,11 @@
+import { HTMLAttributes, useRef, useState } from 'react'
 import NotificationIcon from '../../assets/icons/notificationicon'
 import UserIcon from '../../assets/icons/userIcon'
 import AccountMenu from '../Menu/accountMenu'
-
+import { motion } from "framer-motion"
 const NavbarInto = () => {
+    const [accountMenuState, setOpenAccountMenuState] = useState(false)
+    const accountMenuRef = useRef<HTMLDivElement | null>(null)
     return (
         <nav className='bg-white px-12 py-3 border-b border-gray-400/40'>
             <div className='flex flex-row'>
@@ -32,16 +35,18 @@ const NavbarInto = () => {
                         <span className='after:content-[""] cursor-pointer size-7 after:rounded-full  after:size-1 after:animate-ping after:bg-theme-secondary after:absolute after:right-1 after:top-1 relative'>
                             <NotificationIcon className='size-7  hover:fill-slate-700  fill-slate-500' />
                         </span>
-                        <div className='flex cursor-pointer relative items-center'>
-                            <span className='border inline-flex items-center justify-center m-auto size-7 rounded-full bg-theme-customPrimary p-1 '>
-                                <UserIcon className='size-6 content-[1] relative cursor-pointer fill-white' />
-                            </span>
-                            User
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                            </svg>
+                        <div ref={accountMenuRef} className="">
+                            <motion.div onMouseEnter={() => setOpenAccountMenuState(true)} className='flex cursor-pointer relative items-center'>
+                                <span className='border inline-flex items-center justify-center m-auto size-9 rounded-full bg-theme-customPrimary p-1 '>
+                                    <UserIcon className='size-9 content-[1] relative cursor-pointer fill-white' />
+                                </span>
+                                User
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </motion.div>
+                            {accountMenuState && <AccountMenu setOpen={setOpenAccountMenuState} anchorOrigin={accountMenuRef.current} />}
                         </div>
-                        <AccountMenu />
                     </div>
                 </div>
 
