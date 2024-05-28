9a93f8b83a2a1e3ed1e4eba714164cbec08037a5
ahmadOlawale008 <kassimolwale15@gmail.com>
2024-05-28 06:46:44 +0100

    Categories slider added

--- frontend/myapp/src/pages/homePage/sections/advertisement/imageScrolll.tsx
+++ frontend/myapp/src/pages/homePage/sections/advertisement/imageScrolll.tsx
@@ -5,17 +5,22 @@ import { useScroll } from 'framer-motion'
 const ImageScrolll = () => {
     const {scrollYProgress} = useScroll()
     return (
-        <div className='border-y-4 border-neutal-900 border-slate-200'>
-            <motion.div className="flex w-full overflow-hidden flex-row  justify-evenly items-start">
+        <div className='border-y-4 flex overflow-hidden flex-nowrap border-neutral-700 bg-gray-50'>
+            {/* <marquee behavior="" direction=""></marquee> */}
+            <motion.div className="flex shrink-0 w-full  flex-row  justify-evenly items-start">
                 {ScrollContent.map((d) => <div key={d.id} className='flex flex-1 basis-full flex-col items-center'>
-                    <Button baseClassName='px-3 flex w-full flex-col active:scale-1 active:shadow-none !text-xl' style={{fontWeight: 900}} rounded='none' color='primary' iconClassName='size-9' variant='text'  icon={d.src}>{d.category}</Button>
+                    <Button baseClassName='px-3 flex w-full flex-col py-2 active:scale-1 active:shadow-none font-normal !text-lg tracking-tight leading-loose' rounded='none' color='primary' iconClassName='size-9' variant='text'  icon={d.src}>{d.category}</Button>
                 </div>)}
-                {ScrollContent.map((d) => <div key={d.id} className='flex flex-col items-center justify-center'>
-                    <Button baseClassName='px-3 flex w-full flex-col active:scale-1 active:shadow-none !text-xl' style={{ fontWeight: 900 }} rounded='none' color='primary' iconClassName='size-9' variant='text' icon={d.src}>{d.category}</Button>
+            </motion.div>
+            <motion.div className="flex w-full shrink-0 justify-evenly items-start">
+                {ScrollContent.map((d) => <div key={d.id} className='flex flex-1 basis-full flex-col items-center'>
+                    <Button baseClassName='px-3 flex w-full flex-col py-2 active:scale-1 active:shadow-none font-normal !text-lg tracking-tight leading-loose' rounded='none' color='primary' iconClassName='size-9' variant='text' icon={d.src}>{d.category}</Button>
                 </div>)}
-                {/* {ScrollContent.map((d) => <div key={d.id} className='flex flex-col items-center justify-center'>
-                    <Button baseClassName='px-3 flex  flex-col active:scale-1 active:shadow-none !text-xl' style={{ fontWeight: 900 }} rounded='none' color='primary' iconClassName='size-9' variant='text' icon={d.src}>{d.category}</Button>
-                </div>)} */}
+            </motion.div>
+            <motion.div className="flex w-full shrink-0 justify-evenly items-start">
+                {ScrollContent.map((d) => <div key={d.id} className='flex flex-1 basis-full flex-col items-center'>
+                    <Button baseClassName='px-3 flex w-full flex-col py-2 active:scale-1 active:shadow-none font-normal !text-lg tracking-tight leading-loose' rounded='none' color='primary' iconClassName='size-9' variant='text' icon={d.src}>{d.category}</Button>
+                </div>)}
             </motion.div>
         </div>
     )
