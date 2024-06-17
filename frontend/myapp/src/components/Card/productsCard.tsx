d70893eb4d64801ea2f2df35bb144f243a12196e
ahmadOlawale008 <kassimolwale15@gmail.com>
2024-06-17 21:48:12 +0100

    Changes

--- frontend/myapp/src/components/Card/productsCard.tsx
+++ frontend/myapp/src/components/Card/productsCard.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react'
 import { Player } from "@lordicon/react"
 import Rating, { RatingType, RatingSizeType } from '../Rating/rating-export'
 import Button from '../Button/button'
+import Bookmark from '../../assets/icons/bookmark'
 interface ProductCard {
     link: string
     img: string,
@@ -45,8 +46,11 @@ const ProductCard: React.FC<ProductCard> = ({ link, img, rate, product_name, sta
                 }} className='absolute top-4 align-middle inline-flex items-center justify-center cursor-pointer left-3 z-30 bg-white rounded-full p-1'>
                     {likedPost ? <Player onComplete={() => setLikedPost(false)} ref={iconRef} icon={heartFilledIcon} size={21} colorize='#ef2424'></Player> : <Player ref={iconRef} onComplete={() => setLikedPost(true)} size={21} icon={hearticon}></Player>}
                 </span>
-                <span className='absolute  top-4 right-3 bg-secondary-dark z-30'>
-                    -30%
+                <span className='absolute  top-4 right-3 z-30'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-primary-dark">
+                        <path fillRule="evenodd" d="M5.166 2.621v.858c-1.035.148-2.059.33-3.071.543a.75.75 0 0 0-.584.859 6.753 6.753 0 0 0 6.138 5.6 6.73 6.73 0 0 0 2.743 1.346A6.707 6.707 0 0 1 9.279 15H8.54c-1.036 0-1.875.84-1.875 1.875V19.5h-.75a2.25 2.25 0 0 0-2.25 2.25c0 .414.336.75.75.75h15a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-2.25-2.25h-.75v-2.625c0-1.036-.84-1.875-1.875-1.875h-.739a6.706 6.706 0 0 1-1.112-3.173 6.73 6.73 0 0 0 2.743-1.347 6.753 6.753 0 0 0 6.139-5.6.75.75 0 0 0-.585-.858 47.077 47.077 0 0 0-3.07-.543V2.62a.75.75 0 0 0-.658-.744 49.22 49.22 0 0 0-6.093-.377c-2.063 0-4.096.128-6.093.377a.75.75 0 0 0-.657.744Zm0 2.629c0 1.196.312 2.32.857 3.294A5.266 5.266 0 0 1 3.16 5.337a45.6 45.6 0 0 1 2.006-.343v.256Zm13.5 0v-.256c.674.1 1.343.214 2.006.343a5.265 5.265 0 0 1-2.863 3.207 6.72 6.72 0 0 0 .857-3.294Z" clipRule="evenodd" />
+                    </svg>
+
                 </span>
             </div>
             <div className='ratingu7a block'>
