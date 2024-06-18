32cd66956179b0595be09ff79c40b6ae678603ed
ahmadOlawale008 <kassimolwale15@gmail.com>
2024-06-18 01:51:09 +0100

    Newly added

--- frontend/myapp/tailwind.config.js
+++ frontend/myapp/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,ts,tsx}"],
+  content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {
       colors: {
