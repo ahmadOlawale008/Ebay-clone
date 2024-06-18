55b65f801a1154fd8c9985e091adad91d32be56b
ahmadOlawale008 <kassimolwale15@gmail.com>
2024-06-18 01:49:18 +0100

    Input component added

--- frontend/myapp/tailwind.config.js
+++ frontend/myapp/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
     extend: {
       colors: {
