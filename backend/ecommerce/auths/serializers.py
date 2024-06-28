025fa214feda6d34124646c5b1ca5290c84785e7
ahmadOlawale008 <kassimolwale15@gmail.com>
2024-06-29 00:06:38 +0100

    Googe Auth

--- backend/ecommerce/auths/serializers.py
+++ backend/ecommerce/auths/serializers.py
@@ -8,37 +8,53 @@ from django.conf import settings
 from .models import AccountType, BusinessInfo, PersonalInfo
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import Token
+
+
 class EazeSalesTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
+        token["id"] = user.id
+        token["email"] = user.email
+        token["account_type"] = user.account_type
         try:
-            if user.account_type == AccountType.BUYER and PersonalInfo.objects.get(user=user):
+            if user.account_type == AccountType.BUYER:
                 personal_info = PersonalInfo.objects.get(user=user)
-                token["first_name"] = personal_info.first_name
-                token["last_name"] = personal_info.last_name
-                token["created"] = personal_info.created
-        except Exception as e:
+                token.update(
+                    first_name=personal_info.first_name,
+                    last_name=personal_info.last_name,
+                    created=personal_info.created,
+                )
+            elif user.account_type == AccountType.SELLER:
+                business_info = BusinessInfo.objects.get(user=user)
+                token.update(
+                    business_name=business_info.business_name,
+                    first_name=business_info.first_name,
+                    last_name=business_info.last_name,
+                    created=business_info.created,
+                )
+
+        except (PersonalInfo.DoesNotExist, BusinessInfo.DoesNotExist):
             pass
-        token['id'] = user.id
-        token['first_name'] = user.first_name
-        token['last_name'] = user.last_name
-        token['email'] = user.email
-        token['account_type'] = user.account_type
         return token
-    
+
+
 class UserSerializer(serializers.ModelSerializer):
     def validate_password(value):
         if not any(char in string.ascii_uppercase for char in value):
             return serializers.ValidationError(
                 {
-                    "password": _("Please ensure your password contains upper case for security reasons.")
+                    "password": _(
+                        "Please ensure your password contains upper case for security reasons."
+                    )
                 }
             )
         if not any(char in string.ascii_lowercase for char in value):
             return serializers.ValidationError(
                 {
-                    "password": _("Please ensure your password contains lower case for security reasons.")
+                    "password": _(
+                        "Please ensure your password contains lower case for security reasons."
+                    )
                 }
             )
         if not any(char in string.punctuation for char in value):
@@ -53,7 +69,7 @@ class UserSerializer(serializers.ModelSerializer):
             return serializers.ValidationError(
                 {"message": _("Ensure your password has a minimum length of 8")}
             )
-            
+
     def validate_email(value):
         try:
             django_validate_email(value)
@@ -63,26 +79,46 @@ class UserSerializer(serializers.ModelSerializer):
             return serializers.ValidationError(
                 {"email": "Email Address Already in Use."}
             )
+
     password = serializers.CharField()
     confirm_password = serializers.CharField()
+
     def validate_first_name(self, value):
-        if value.strip() == '' or  len(value.strip()) == 0:
-            return serializers.ValidationError({"first_name": "Please provide your first name."})
+        if value.strip() == "" or len(value.strip()) == 0:
+            return serializers.ValidationError(
+                {"first_name": "Please provide your first name."}
+            )
+
     def validate_last_name(self, value):
-        if value.strip() == '' or len(value.strip()) == 0:
-            return serializers.ValidationError({"last_name": "Please provide your last name."})
+        if value.strip() == "" or len(value.strip()) == 0:
+            return serializers.ValidationError(
+                {"last_name": "Please provide your last name."}
+            )
+
     def validate_phone_number(self, value):
         if get_user_model().objects.filter(phone_number=value).exists():
-            return serializers.ValidationError({"phone_number": "A user with this username already exists."})
-        if value.strip() == '' or len(value.strip()) == 0:
-            return serializers.ValidationError({"phone_number": "Please provide a valid phone number."})
+            return serializers.ValidationError(
+                {"phone_number": "A user with this username already exists."}
+            )
+        if value.strip() == "" or len(value.strip()) == 0:
+            return serializers.ValidationError(
+                {"phone_number": "Please provide a valid phone number."}
+            )
+
     def validate(self, attrs):
         password = attrs.get("password", "")
         confirm_password = attrs.get("confirm_password", "")
         if confirm_password != password:
-            return serializers.ValidationError({"confirm_password": "Ensure that both passwords are equal"})
+            return serializers.ValidationError(
+                {"confirm_password": "Ensure that both passwords are equal"}
+            )
         return super().validate(attrs)
+
     class Meta:
         model = get_user_model()
         fields = ["id", "first_name", "last_name", "email", "phoneNumber", "password"]
-        extra_fields = {"read_only": ["id", ]}
+        extra_fields = {
+            "read_only": [
+                "id",
+            ]
+        }
